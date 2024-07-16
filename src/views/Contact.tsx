2a1890a47c23a69556c84dd751cf87e4059089c2
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2024-07-16 15:23:16 +0200

    chore: typo error

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -17,7 +17,7 @@ export default function ContactView() {
         <Typography>François Huart</Typography>
         <Typography>11, rue saules Bastin</Typography>
         <Typography>4920 Remouchamps</Typography>
-        <Typography>Belgiqye</Typography>
+        <Typography>Belgique</Typography>
         <Typography>Tel: +32 (0) 4 384 58 22</Typography>
         <Typography>Email: francois.huart@gmail.com</Typography>
       </CardContent>
