59e8e88cbb3fda771a23728009f8a77db04302bb
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2024-07-17 08:32:12 +0200

    feature: inventaire

---
+++ src/interfaces/InventaireInterface.tsx
@@ -0,0 +1,4 @@
+export interface InventaireInterface {
+  section: string;
+  items: string[];
+}
--- src/views/Inventaire.tsx
+++ src/views/Inventaire.tsx
@@ -1,6 +1,26 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  List,
+  ListItem,
+  Grid,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import inventaireData from "../datas/inventaire";
 
 export default function InventaireView() {
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange =
+    (section: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? section : false);
+    };
+
   return (
     <Card id="inventaire" sx={{ padding: 2, marginBottom: 3 }}>
       <CardContent>
@@ -14,6 +34,38 @@ export default function InventaireView() {
         >
           Inventaire du matériel
         </Typography>
+        {inventaireData.map((inventaire) => (
+          <Accordion
+            key={inventaire.section}
+            expanded={expanded === inventaire.section}
+            onChange={handleChange(inventaire.section)}
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography sx={{ fontWeight: "bold" }}>
+                {inventaire.section}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container spacing={2}>
+                {[...Array(Math.ceil(inventaire.items.length / 10))].map(
+                  (_, colIndex) => (
+                    <Grid item xs={6} key={colIndex}>
+                      <List>
+                        {inventaire.items
+                          .slice(colIndex * 10, (colIndex + 1) * 10)
+                          .map((item, rowIndex) => (
+                            <ListItem key={rowIndex} sx={{ padding: 0 }}>
+                              {item}
+                            </ListItem>
+                          ))}
+                      </List>
+                    </Grid>
+                  )
+                )}
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        ))}
       </CardContent>
     </Card>
   );
